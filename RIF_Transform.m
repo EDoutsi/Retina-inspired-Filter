25e4267c1ef7763632b8963856b6321875ba6cb7
EDoutsi <58698098+EDoutsi@users.noreply.github.com>
2019-12-09 19:36:48 +0200

    Update RIF_Transform.m

--- RIF_Transform.m
+++ RIF_Transform.m
@@ -11,21 +11,13 @@
 close all; clc;
 
 %% SET OF BIO-PLAUSIBLE PARAMETERS 
-sc  = 0.5;       % standard deviation sc = 0.5 according to Wohrer et al. 
-ss  = 3*sc;      % bio-plausible ss = 3*sc according to Wohrer et al.
-max = 10;        % 4 in the experiments
-min  = -max;  
-samp = 50*max+1; % 100*max+1 in the experiments
-tauC = 20.*10^-3;
-tauS = 4.*10^-3;
-tauG = 5.*10^-3;
-wC = 1;          % weight  center/ constant valq  ue wC=0.75
-wS = 1;          % weight surround
-T  = 150;        % bio-plausible processing time
-tmax = T;        % i) t = T or ii) t > T  (the difference is in the convergence)
-tsamp = 1;       % number od time instances
-t1 = 1:tsamp:tmax;              % temporal vector
-x  = linspace(min,max,samp);    % spatial vector
+sc  = 0.5; ss  = 3*sc;      % bio-plausible parameters 
+max = 10; min  = -max; samp = 50*max+1; % 100*max+1 in the experiments
+tauC = 20.*10^-3; tauS = 4.*10^-3; tauG = 5.*10^-3;
+wC = 1; wS = 1;             % weight surround
+T  = 150;  tmax = T;  tsamp = 1;    % number odd time instances
+t1 = 1:tsamp:tmax;                  % temporal vector
+x  = linspace(min,max,samp);        % spatial vector
 
 %% BUILD THE SPATIAL AND TEMPORAL FILTERS
 [Filter,fftFilter] = RIF_Kernel(sc,ss,max,min,samp,tsamp,tauC,tauS,tauG,wC,wS,T,tmax,t1);
