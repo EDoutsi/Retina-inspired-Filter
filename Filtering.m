bf85392169a4c571652950fd9a77d27b6ebb3d3d
EDoutsi <58698098+EDoutsi@users.noreply.github.com>
2020-01-29 16:32:22 +0200

    Update Filtering.m

--- Filtering.m
+++ Filtering.m
@@ -7,29 +7,23 @@
 %           3. RIF Filter "Filter" in space domain
 %           4. "noise" (default 0)
 % ------------------------------------------------------------------------
-% ALGORITHM --> We test the size of the two matrices "I" and "Filter". If they have the same
-% size then we don't need to add any zero padding. As a result, we use the 
-% fft2 (fast fourier transform for 2D) in the default way. In case the two 
-% matrices have different size we need to do the zero-padding                 
+% ALGORITHM --> We test the size of the two matrices "I" and "Filter". 
+% If they have the same size then we don't need to add any zero padding. 
+% As a result, we use the fft2 (fast fourier transform for 2D) in the default 
+% way. In case the two matrices have different size we need to do the zero-padding.                 
 % ------------------------------------------------------------------------
 % OUTPUT --> 1. The filtered images "obs_Matrix" in space domain
 %            2. The filtered images "fftobs_Matrix" in frequency domain
 %            3. The zero-padded filter "fftF_Matrix" in frequency domain
 % ------------------------------------------------------------------------
-
 function  [obs_Matrix,fftobs_Matrix,fftF_Matrix] = Filtering(I,fftFilter, Filter, noise)
         [Ix,Iy] = size(I);
         [Fx,Fy] = size(Filter);
         for i = 1:Fx                    
-                F = Matrix_reshape(Filter,i,sqrt(Fy),sqrt(Fy));                          % extract 1-1 the decomposition layers without noise
-                fftFSpectrum = Matrix_reshape(fftFilter,i,sqrt(Fy),sqrt(Fy));            % extract 1-1 the decomposition layers without noise
+                F = Matrix_reshape(Filter,i,sqrt(Fy),sqrt(Fy));                  
+                fftFSpectrum = Matrix_reshape(fftFilter,i,sqrt(Fy),sqrt(Fy));    
                 if (Iy ~= Fy)
                     %% COMPUTE THE CONVOLUTION BETWEEN THE FILTER AND THE IMAGE
-                    % We actually use the mulitplication in fourier domain instead of the 
-                    % convolution in space. We compute the fft of the input image I 
-                    % and the filter F. Finally, we calculate the (fftobs) and we inverse
-                    % this to extract the observations in space. Finally, we save the
-                    % obs and the fftobs results in matrices.
                     h = F;
                     ffth = fftFSpectrum;
                     s = (size(fftFSpectrum)-1)/2;
@@ -44,17 +38,11 @@ function  [obs_Matrix,fftobs_Matrix,fftF_Matrix] = Filtering(I,fftFilter, Filter
                     F = fftshift(Hs);
                     fftI = fft2(I);
                     fftF = fft2(F);
-
                    %% CONVOLUTION in SPACE = MULTIPLICATION in FOURIER                          
-                    fftWhite = fft2(noise*randn(Ix,Iy));
+                    fftWhite = fft2(noie*randn(Ix,Iy));
                     fftobsSpectrum = fftI.*fftFSpectrum;            % using the Gaussian in frequency 
                     fftobsSpace      = fftI.* fftF + fftWhite;                 % using the Gaussian in space
                     obs = real((ifft2(fftobsSpace))); % using the Gaussian in space
-% %                     figure(1);
-% %                     imagesc(obs);colormap(gray);
-                    %% NOISE
-% %                     obsFreq = obsFreq + noise*randn(Ix,Iy);     
-% %                     fftobs = fft2(obsFreq);
                 else
                     fftI = fft2(I);
                     fftobsSpace = fftI.*fftF;
@@ -63,26 +51,4 @@ function  [obs_Matrix,fftobs_Matrix,fftF_Matrix] = Filtering(I,fftFilter, Filter
                 fftobs_Matrix(i,:) = fftobsSpace(:);
                 fftF_Matrix(i,:) = fftF(:);
         end
-        
-% %     figure(2);
-% %     subplot(2,3,1);
-% %     imagesc(I);colormap(gray);
-% %     title('Original Signal');
-% %     subplot(2,3,2);
-% %     mesh(F0);
-% %     title('Filter in space');
-% %     subplot(2,3,3);
-% %     imagesc(obs);colormap(gray);
-% %     title('Observations');
-% %     subplot(2,3,4);
-% %     imagesc(log(abs(fftshift(fftI))));colormap(gray);
-% %     title('FFT of the signal');
-% %     subplot(2,3,5);
-% %     imagesc(log(abs(fftshift(fftF))));
-% %     title('FFT of the filter');
-% %     subplot(2,3,6);
-% %     imagesc(log(abs(fftshift(fftobs))));colormap(gray);
-% %     title('FFT of the observation');
-% %     
-    
 end
